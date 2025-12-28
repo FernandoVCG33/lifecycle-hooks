681203c26686a6ec95bad9068b031dc6ab642a9e
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-28 17:02:38 -0400

    Implement lifecycle hooks in HomePage component

--- hooks/src/app/pages/home-page/home-page.ts
+++ hooks/src/app/pages/home-page/home-page.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   imports: [],
   templateUrl: './home-page.html',
 })
-export class HomePage {
+export class HomePage implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  constructor() {
+    console.log('Constructor llamado');
+    // afterNextRender y afterEveryRender se usan aquí
+  }
 
+  ngOnInit() {
+    console.log('ngOnInit llamado');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges llamado', changes);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck llamado');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit llamado');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked llamado');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit llamado');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked llamado');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy llamado');
+  }
 }

ad0f48e9d7dab1428d7bc960e53b64b6c4e94894
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-28 16:54:34 -0400

    Add routing and navbar components; create home, contact, and about pages

--- hooks/src/app/app.routes.ts
+++ hooks/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import {HomePage} from './pages/home-page/home-page';
+import {ContactPage} from './pages/contact-page/contact-page';
+import {AboutPage} from './pages/about-page/about-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component: HomePage,
+  },
+  {
+    path: 'contact',
+    component: ContactPage,
+    title: 'Contacto'
+  },
+  {
+    path: 'about',
+    component: AboutPage,
+    title: 'About us'
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+
+];
